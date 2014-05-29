d828cfe98628810c82abde9e17be178201f7b51f
Parashar Bhatt <pb.tech2010@gmail.com>
2014-05-29 13:50:03 -0400

    Cleanup client_stub

--- client_stub.c
+++ client_stub.c
@@ -21,15 +21,8 @@ return_type make_remote_call(
     const int nparams,
     ...)
 {
-    printf("%s to %s\n", "make call", procedure_name);
-
-    struct sockaddr_in server;
-    int s, i, slen=sizeof(server);
     char buf[BUFLEN] = "";
     char* buf_position = buf;
-
-    va_list valist;
-    arg_type args;
 
     // Copy procedure name to buf
     strcpy(buf_position, procedure_name);
@@ -39,38 +32,35 @@ return_type make_remote_call(
     memcpy(buf_position, &nparams, sizeof(nparams));
     buf_position += sizeof(nparams);
 
+    /* Access params based on nparams */
+    va_list valist;
     va_start(valist, nparams);
 
-    /* access all the arguments assigned to valist */
+    int i;
     for (i = 0; i < nparams; i++)
     {
-        // copy size of param to buf
+        // Copy size of param to buf
         int size = va_arg(valist, int);
         memcpy(buf_position, &size, sizeof(int));
         buf_position += sizeof(int);
 
-        // copy param to buf
+        // Copy param to buf
         void* param = va_arg(valist, void*);
         memcpy(buf_position, param, size);
         buf_position += size;
     }
 
-    /* clean memory reserved for valist */
+    // Clean memory reserved for valist
     va_end(valist);
 
-    // Print buf
-    /*
-    for (i = 0; i < BUFLEN; i++) {
-        printf("%i ", buf[i]);
-        if (i % 20 == 0 && i > 0) {
-            printf("\n");
-        }
-    }*/
-    
+    int s;
     if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP))==-1) {
-        perror("socket");
+        perror("socket()");
     }
 
+    // Build sockaddr_in
+    struct sockaddr_in server;
+    int slen=sizeof(server);
     memset((char *) &server, 0, sizeof(server));
     server.sin_family = AF_INET;
     server.sin_port = htons(PORT);
@@ -88,7 +78,7 @@ return_type make_remote_call(
     memset(buf, 0, BUFLEN);
     buf_position = buf;
 
-    if (recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &server, &slen)==-1) {
+    if (recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *)&server, &slen)==-1) {
         perror("recvfrom()");
     }
 
