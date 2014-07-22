de377f448455e7f04ad336f6173419ac813d1308
varunsdave <varun.s.dave.pubic@gmail.com>
2014-07-22 11:50:02 -0400

    adding the latest code from professor which make fs_client a legitimate test case and updates helper.c in simplified_rpc

--- ece454a4/fs_client.c
+++ ece454a4/fs_client.c
@@ -31,15 +31,13 @@ void printBuf(char *buf, int size) {
 }
 
 int main(int argc, char *argv[]) {
-    char *dirname = NULL;
-
-    if(argc > 1) dirname = argv[1];
-    else {
-	dirname = (char *)calloc(strlen(".")+1, sizeof(char));
-	strcpy(dirname, ".");
+    if(argc < 4) {
+	fprintf(stderr, "usage: %s <srv-ip/name> <srv-port> <local dir name>\n", argv[0]);
+	exit(1);
     }
 
-    printf("fsMount(): %d\n", fsMount(NULL, 0, dirname));
+    char *dirname = argv[3];
+    printf("fsMount(): %d\n", fsMount(argv[1], atoi(argv[2]), dirname));
     FSDIR *fd = fsOpenDir(dirname);
     if(fd == NULL) {
 	perror("fsOpenDir"); exit(1);
@@ -56,33 +54,34 @@ int main(int argc, char *argv[]) {
 
     printf("fsCloseDir(): %d\n", fsCloseDir(fd));
 
-    int ff = fsOpen("/dev/urandom", 0);
+    int ff = open("/dev/urandom", 0);
     if(ff < 0) {
-	perror("fsOpen"); exit(1);
+	perror("open(/dev/urandom)"); exit(1);
     }
-    else printf("fsOpen(): %d\n", ff);
+    else printf("open(): %d\n", ff);
 
-    char fname[15];
-    if(fsRead(ff, (void *)fname, 10) < 0) {
-	perror("fsRead"); exit(1);
+    char fname[256];
+    sprintf(fname, "%s/", dirname);
+    if(read(ff, (void *)(fname+strlen(dirname)+1), 10) < 0) {
+	perror("read(/dev/urandom)"); exit(1);
     }
 
     int i;
     for(i = 0; i < 10; i++) {
 	//printf("%d\n", ((unsigned char)(fname[i]))%26);
-	fname[i] = ((unsigned char)(fname[i]))%26 + 'a';
+	fname[i+strlen(dirname)+1] = ((unsigned char)(fname[i+strlen(dirname)+1]))%26 + 'a';
     }
-    fname[10] = (char)0;
+    fname[10+strlen(dirname)+1] = (char)0;
     printf("Filename to write: %s\n", (char *)fname);
 
     char buf[256];
-    if(fsRead(ff, (void *)buf, 256) < 0) {
-	perror("fsRead(2)"); exit(1);
+    if(read(ff, (void *)buf, 256) < 0) {
+	perror("read(2)"); exit(1);
     }
 
     printBuf(buf, 256);
 
-    printf("fsClose(): %d\n", fsClose(ff));
+    printf("close(): %d\n", close(ff));
 
     ff = fsOpen(fname, 1);
     if(ff < 0) {
@@ -97,7 +96,33 @@ int main(int argc, char *argv[]) {
 	perror("fsClose"); exit(1);
     }
 
+    char readbuf[256];
+    if((ff = fsOpen(fname, 0)) < 0) {
+	perror("fsOpen(read)"); exit(1);
+    }
+
+    int readcount = -1;
+
+    if((readcount = fsRead(ff, readbuf, 256)) < 256) {
+	fprintf(stderr, "fsRead() read fewer than 256\n");
+    }
+
+    if(memcmp(readbuf, buf, readcount)) {
+	fprintf(stderr, "return buf from fsRead() differs from data written!\n");
+    }
+    else {
+	printf("fsread(): return buf identical to data written upto %d bytes.\n", readcount);
+    }
+
+    if(fsClose(ff) < 0) {
+	perror("fsClose"); exit(1);
+    }
+
     printf("fsRemove(%s): %d\n", fname, fsRemove(fname));
+
+    if(fsUnmount(dirname) < 0) {
+	perror("fsUnmount"); exit(1);
+    }
 
     return 0;
 }
